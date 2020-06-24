081c8e481963a77d15106a6017b8767e301a016d
MartinVigliarolo <martin.vigliarolo@gmail.com>
2020-06-24 20:25:46 -0300

    Primer problema de spoj

--- Part1/Ex1.py
+++ Part1/Ex1.py
@@ -1,2 +1,16 @@
-print ("Name: Martin Vigliarolo \n")
-print("Email: martin.vigliarolo@gmail.com \n")
+# Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. 
+# More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42.
+# All numbers at input are integers of one or two digits.
+
+EsCuatroDos = False
+elementos = []
+
+while not EsCuatroDos:
+    entrada = input()
+    if int(entrada) == 42:
+        EsCuatroDos = True
+    else:
+        elementos.append(int(entrada))
+
+for elem in elementos:
+    print(elem)
