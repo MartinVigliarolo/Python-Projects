826d1ee6dfcdff646afbf7f3ed2235a43c4f2beb
MartinVigliarolo <martin.vigliarolo@gmail.com>
2020-06-24 21:54:58 -0300

    No empezado ejercicio 3

---
+++ Part1/Ex3.py
@@ -0,0 +1,12 @@
+#  Transform the algebraic expression with brackets into RPN form (Reverse Polish Notation). Two-argument operators: +, -, *, /, ^ (priority from the lowest to the highest), brackets ( ). Operands: only letters: a,b,...,z. Assume that there is only one RPN form (no expressions like a*b*c).
+# Input
+
+# t [the number of expressions <= 100]
+# expression [length <= 400]
+# [other expressions]
+
+# Text grouped in [ ] does not appear in the input file.
+# Output
+
+# The expressions in RPN form, one per line.
+
