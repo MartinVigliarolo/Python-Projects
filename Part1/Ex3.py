0caa2a818a3c6277cfd3585579f956ca5ad6bd62
MartinVigliarolo <martin.vigliarolo@gmail.com>
2020-06-25 23:40:56 -0300

    Problema de numeros palindromos sin terminar

--- Part1/Ex3.py
+++ Part1/Ex3.py
@@ -1,12 +1,44 @@
-#  Transform the algebraic expression with brackets into RPN form (Reverse Polish Notation). Two-argument operators: +, -, *, /, ^ (priority from the lowest to the highest), brackets ( ). Operands: only letters: a,b,...,z. Assume that there is only one RPN form (no expressions like a*b*c).
+# A positive integer is called a palindrome if its representation in the decimal system is the same when read from left to right and from right to left. For a given positive integer K of not more than 1000000 digits, write the value of the smallest palindrome larger than K to output. Numbers are always displayed without leading zeros.
 # Input
 
-# t [the number of expressions <= 100]
-# expression [length <= 400]
-# [other expressions]
-
-# Text grouped in [ ] does not appear in the input file.
+# The first line contains integer t, the number of test cases. Integers K are given in the next t lines.
 # Output
 
-# The expressions in RPN form, one per line.
+# For each K, output the smallest palindrome larger than K.
+# Example
+
+# Input:
+# 2
+# 808
+# 2133
+
+# Output:
+# 818
+# 2222
+
+import math
+
+def getDigits(ent):
+    return math.floor(math.log(int(ent),10)+1)
+
+t = input()
+entradas = []
+
+for i in range(int(t)):
+    entradas.append(input())
+
+for ent in entradas:
+    digits = getDigits(ent)
+    ret = ''
+    if digits == 1:
+        print(ent)
+    else:
+        while digits > 1:
+            digitoMayor = math.floor(int(ent)/10*(digits-1))
+            # digitoMenor = 
+            ret = str(digitoMayor) + ret + str(digitoMayor)
+            digits = digits - 2
+        print(ret)
+
+
 
